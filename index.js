fe8eaf30171a7ececb4f9fc6d5631faa8564e251
devops4sure <suchethgr@gmail.com>
2026-02-06 14:47:11 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT ??  8080
+
+app.get('/',(req,res)=>{
+    return res.json({
+        msg:"Hello from the server"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is up and running on port ${PORT}`)
+})
